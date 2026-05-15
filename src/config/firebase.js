a89941f2fc643662379de96750b3fa04f3a13aa9
Mr.Morsing14 <frmo0006@stud.ek.dk>
2026-05-15 05:01:29 +0200

    cleanup

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -5,12 +5,12 @@ import { getStorage } from "firebase/storage";
 
 // TODO: Replace with your Firebase project config
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
 };
 
 const app = initializeApp(firebaseConfig);
